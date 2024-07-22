a1e64f71f593e5f500e85519ec6df55b3017a133
Joaquin Metalabs <joaquin@metalabs.company>
2024-07-22 22:09:39 +0700

    Fix: order lan 3

--- src/pages/orders/index.tsx
+++ src/pages/orders/index.tsx
@@ -2,10 +2,13 @@ export default function Orders(){
     return (
         <div>
             <div>Order haahaha</div>
-            <p>Order hihihi</p>
 
 
             
+            <p>Order hihihi</p>
+
+
+
         </div>
     )
 }
